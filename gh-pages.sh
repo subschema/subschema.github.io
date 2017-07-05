3c57451438dae9779ee2e84e6a03a6727283729c
Justin Spears <speajus@gmail.com>
2017-07-05 11:45:58 -0700

    update gh-pages

--- gh-pages.sh
+++ gh-pages.sh
@@ -6,13 +6,12 @@
 # upstream        git@github.com:subschema/subschema-devel.git
 # website git@github.com:subschema/subschema.github.io.git
 # wiki    git@github.com:subschema/subschema.wiki.git
-
-git subtree split --prefix docs -b gh-pages && \
 git add docs && \
 git commit -a -m 'publish docs' && \
+git subtree split --prefix docs -b gh-pages && \
 git push -f website gh-pages:master && \
-git branch -D gh-pages
-
+git branch -D gh-pages && \
+git rm -rf --cached docs
 #git subtree split --prefix subschema.wiki/src -b wiki && \
 #git push -f wiki wiki:master && \
 #git branch -d wiki
